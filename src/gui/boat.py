8b136141bcffed6becf8014a3c9bf64edf51dcca
Casper <133casper@gmail.com>
2021-02-20 22:16:34 +0300

    feat: add keyboard actions: increasing/decreasing speed, moving left and moving right (ACOP-2)

--- src/gui/boat.py
+++ src/gui/boat.py
@@ -1,22 +1,44 @@
 from typing import Final
 
-STEP_SIZE: Final[int] = 10
+CHANGE_SPEED_STEP: Final[float] = 0.0001
+CHANGE_DIRECTION_STEP: Final[float] = 0.05
+INITIAL_BOAT_SPEED: Final[float] = 0.05
 
-x_coordinate: int
-y_coordinate: int
+x_coordinate: float
+y_coordinate: float
+boat_speed: float = INITIAL_BOAT_SPEED
 
 
-def init_coordinates(init_x_coordinate: int, init_y_coordinate: int) -> None:
+def init_coordinates(init_x_coordinate: float, init_y_coordinate: float) -> None:
     global x_coordinate, y_coordinate
-
     x_coordinate = init_x_coordinate
     y_coordinate = init_y_coordinate
 
 
-def get_current_coordinates() -> tuple[int, int]:
+def get_current_coordinates() -> tuple[float, float]:
     return x_coordinate, y_coordinate
 
 
-def turn_right() -> None:
+def go_ahead() -> None:
+    global y_coordinate
+    y_coordinate += boat_speed
+
+
+def increase_speed() -> None:
+    global boat_speed
+    boat_speed += CHANGE_SPEED_STEP
+
+
+def decrease_speed() -> None:
+    global boat_speed
+    boat_speed -= CHANGE_SPEED_STEP
+
+
+def move_left() -> None:
     global x_coordinate
-    x_coordinate += STEP_SIZE
+    x_coordinate -= CHANGE_DIRECTION_STEP
+
+
+def move_right() -> None:
+    global x_coordinate
+    x_coordinate += CHANGE_DIRECTION_STEP
--- src/gui/gui_utils.py
+++ src/gui/gui_utils.py
@@ -11,7 +11,7 @@ BACKGROUND_COLOR: Final[Color] = Color(156, 192, 249)
 WINDOW_CAPTION: Final[str] = "Boat"
 
 INITIAL_X_COORDINATE: Final[int] = X_WINDOW_SIZE // 2
-INITIAL_Y_COORDINATE: Final[int] = Y_WINDOW_SIZE
+INITIAL_Y_COORDINATE: Final[int] = 0
 
 BOAT_COLOR: Final[Color] = Color(34, 139, 34)
 BOAT_CIRCLE_RADIUS: Final[int] = 20
@@ -29,18 +29,42 @@ def execute_run_loop() -> None:
     draw_boat()
 
     run_loop = True
+    move_ticker = 0
 
     while run_loop:
+        boat.go_ahead()
+
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[pygame.K_UP]:
+            move_ticker = 60
+            boat.increase_speed()
+        elif pressed_keys[pygame.K_DOWN]:
+            move_ticker = 60
+            boat.decrease_speed()
+        elif pressed_keys[pygame.K_LEFT]:
+            move_ticker = 60
+            boat.move_left()
+        elif pressed_keys[pygame.K_RIGHT]:
+            move_ticker = 60
+            boat.move_right()
+
+        if move_ticker > 0:
+            move_ticker -= 1
+
+        pygame.display.get_surface().fill(BACKGROUND_COLOR)
+        draw_boat()
         pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run_loop = False
 
 
 def draw_boat():
-    pygame.display.get_surface().fill(BACKGROUND_COLOR)
-
     boat_coordinates = boat.get_current_coordinates()
+    boat_coordinates = convert_to_pygame_coordinates(boat_coordinates)
+
     pygame.draw.circle(
         pygame.display.get_surface(),
         BOAT_COLOR,
@@ -48,4 +72,6 @@ def draw_boat():
         BOAT_CIRCLE_RADIUS
     )
 
-    pygame.display.flip()
+
+def convert_to_pygame_coordinates(coordinates: tuple[float, float]) -> tuple[float, float]:
+    return coordinates[0], Y_WINDOW_SIZE - coordinates[1]
