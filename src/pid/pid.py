4756fcc56c22d05110f5e785fef5aaeaaa6449f0
Konstantin Shulga <mrkonstantinsh@gmail.com>
2021-03-03 22:32:44 +0300

    feat: add windup guard for angle & change gains (ACOP-8)

--- src/pid/pid.py
+++ src/pid/pid.py
@@ -3,13 +3,13 @@ from typing import Final
 
 UPDATE_INTERVAL_TIME: Final[float] = 0.01
 
-VELOCITY_PROPORTIONAL_GAIN: Final[float] = 0.5
-VELOCITY_INTEGRAL_GAIN: Final[float] = 0.0001
-VELOCITY_DERIVATIVE_GAIN: Final[float] = 0.2
+VELOCITY_PROPORTIONAL_GAIN: Final[float] = 0.60
+VELOCITY_INTEGRAL_GAIN: Final[float] = 0.00022
+VELOCITY_DERIVATIVE_GAIN: Final[float] = 0.36
 
-DEGREE_PROPORTIONAL_GAIN: Final[float] = 1.2
-DEGREE_INTEGRAL_GAIN: Final[float] = 1.0
-DEGREE_DERIVATIVE_GAIN: Final[float] = 0.001
+DEGREE_PROPORTIONAL_GAIN: Final[float] = 1.8
+DEGREE_INTEGRAL_GAIN: Final[float] = 0.003
+DEGREE_DERIVATIVE_GAIN: Final[float] = 1.20
 
 VELOCITY_WINDUP_GUARD: Final[float] = 95.0
 DEGREE_WINDUP_GUARD: Final[float] = 25.0
@@ -23,6 +23,7 @@ velocity_integral_term: list[float] = [0.0]
 degree_integral_term: list[float] = [0.0]
 
 current_velocity_integral_gain: float = VELOCITY_INTEGRAL_GAIN
+current_degree_integral_gain: float = DEGREE_INTEGRAL_GAIN
 
 
 def update_pid(delta_time: float, deviation_from_course: float, distance_to_target: float) -> tuple[float, float]:
@@ -41,6 +42,7 @@ def update_pid(delta_time: float, deviation_from_course: float, distance_to_targ
     global degree_integral_term
 
     global current_velocity_integral_gain
+    global current_degree_integral_gain
 
     print(f"DISTANCE ERROR: {distance_to_target}")
     print(f"DEVIATION ERROR: {deviation_from_course}")
@@ -63,15 +65,10 @@ def update_pid(delta_time: float, deviation_from_course: float, distance_to_targ
 
     print(f"BOAT VELOCITY before WINDUP GUARD: {boat_velocity}")
 
-    # integral windup: https://youtu.be/NVLXCwc8HzM?t=200
-    if boat_velocity > VELOCITY_WINDUP_GUARD:
-        limited_boat_velocity = VELOCITY_WINDUP_GUARD
-    elif boat_velocity < -VELOCITY_WINDUP_GUARD:
-        limited_boat_velocity = -VELOCITY_WINDUP_GUARD
-    else:
-        limited_boat_velocity = boat_velocity
+    limited_boat_velocity = apply_windup_guard(boat_velocity, VELOCITY_WINDUP_GUARD)
 
-    if not math.isclose(boat_velocity, limited_boat_velocity) and ((boat_velocity > 0) == (distance_to_target > 0)):
+    if not math.isclose(boat_velocity, limited_boat_velocity) \
+            and ((boat_velocity > 0) == (distance_to_target > 0)):
         current_velocity_integral_gain = 0
     else:
         current_velocity_integral_gain = VELOCITY_INTEGRAL_GAIN
@@ -86,11 +83,19 @@ def update_pid(delta_time: float, deviation_from_course: float, distance_to_targ
         delta_deviation_from_course
     )
 
+    limited_steering_wheel_angle = apply_windup_guard(steering_wheel_angle, DEGREE_WINDUP_GUARD)
+
+    if not math.isclose(steering_wheel_angle, limited_steering_wheel_angle) \
+            and ((steering_wheel_angle > 0) == (deviation_from_course > 0)):
+        current_degree_integral_gain = 0
+    else:
+        current_degree_integral_gain = DEGREE_INTEGRAL_GAIN
+
     prev_deviation_from_course = deviation_from_course
     prev_distance_to_target = distance_to_target
     prev_output_value = limited_boat_velocity, steering_wheel_angle
 
-    return limited_boat_velocity, steering_wheel_angle
+    return limited_boat_velocity, limited_steering_wheel_angle
 
 
 def calculate_pid_output(proportional_gain: float, integral_gain: float, derivative_gain: float,
@@ -102,6 +107,16 @@ def calculate_pid_output(proportional_gain: float, integral_gain: float, derivat
     derivative_term = derivative_gain * (delta_error / delta_time)
 
     return proportional_term + integral_gain * integral_term[0] + derivative_term
+
+
+# Integral windup: https://youtu.be/NVLXCwc8HzM?t=200
+def apply_windup_guard(calculated_value: float, windup_guard_value: float) -> float:
+    if calculated_value > windup_guard_value:
+        return windup_guard_value
+    elif calculated_value < -windup_guard_value:
+        return -windup_guard_value
+    else:
+        return calculated_value
 
 
 def reset_pid() -> None:
